ba6d2c80329da8fbbd351b4c3cfb06a11b4c17f2
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-18 20:59:43 +0800

    加入專案檔案。

---
+++ Exec_DeclareString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//第一題
+			Console.WriteLine("c:\\temp\\a.jpg");
+
+			//第二題
+			Console.WriteLine("我是一個\"好學生\"");
+
+			//第三題
+			Console.WriteLine("我是第一行\r\n我是第二行");
+		}
+	}
+}
